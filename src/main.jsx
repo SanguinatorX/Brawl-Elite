7d7d3855646ea0174460beb80a71c5d49243cf6e
SanguinatorX <romangurin01@gmail.com>
2026-04-17 10:30:09 +0200

    added new style file for bases

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import { Provider } from 'react-redux';
 import { store } from './store.js';
 
 import './index.css';
+import './base.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
